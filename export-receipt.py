4ed4d5488ea683855dd751a4e35d6fe9b8a9f436
antma <antma@users.noreply.github.com>
2026-02-18 14:48:20 +0300

    output into csv.gz file

--- export-receipt.py
+++ export-receipt.py
@@ -16,11 +16,16 @@ import log
 import pdf_utils
 import tsv
 
+OUTPUT_DIR = 'output'
+
+if not os.path.lexists(OUTPUT_DIR):
+  os.mkdir(OUTPUT_DIR)
+
 log.init_logging(None, logging.INFO)
 
 json_configuration_filename = os.path.join('conf', 'schema.json')
 json_configuration = tsv.load_json_configuration(json_configuration_filename)
-output_csv_filename = 'receipt.csv'
+output_csv_filename = os.path.join(OUTPUT_DIR, 'receipt.csv.gz')
 
 series = []
 for filename in sorted(glob.glob(os.path.join('input', 'receipt', '*.pdf'))):
@@ -32,4 +37,4 @@ for filename in sorted(glob.glob(os.path.join('input', 'receipt', '*.pdf'))):
 
 df = pd.DataFrame.from_records(series)
 print(df)
-df.to_csv(output_csv_filename)
+df.to_csv(output_csv_filename, compression={'method': 'gzip', 'compresslevel': 9})
